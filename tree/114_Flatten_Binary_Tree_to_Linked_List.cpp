13c062c3b9c0e8d08485132a894fdffe0a39d6e0
Techsailor He <techsailor@163.com>
2017-05-07 17:29:29 +0800

    Add new files

---
+++ tree/114_Flatten_Binary_Tree_to_Linked_List.cpp
@@ -0,0 +1,140 @@
+/*
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+*/
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+/**
+ * Definition for binary tree
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root) {
+            return;
+        }
+        vector<TreeNode*> seq_nodes;
+        travel(root, seq_nodes);
+        seq_nodes.push_back(nullptr);
+        for(int i = 0; i < seq_nodes.size() - 1; i++) {
+            seq_nodes[i]->left = nullptr;
+            seq_nodes[i]->right = seq_nodes[i + 1];
+        }
+    }
+private:
+    void travel(TreeNode *root, vector<TreeNode*> &seq_nodes) {
+        if(!root) {
+            return;
+        }
+        seq_nodes.push_back(root);
+        travel(root->left, seq_nodes);
+        travel(root->right, seq_nodes);
+    }
+};
+
+class BSTree {
+public:
+    BSTree():root(nullptr),size(0) {}
+    int insert(int d);
+    TreeNode* search(int d, TreeNode** parent);
+    int deleteNode(TreeNode* node);
+    void print();
+    TreeNode* root;
+private:
+    int size;
+    void do_print(TreeNode* begin);
+};
+
+int BSTree::insert(int d) {
+    TreeNode* pnode = nullptr;
+    TreeNode* new_node = new TreeNode(d);
+    TreeNode* node = search(d, &pnode);
+    if(!pnode) {
+        root = new_node;
+        return 0;
+    }
+    if(!node) {
+        if(d <= pnode->val) {
+            pnode->left = new_node;
+        }else {
+            pnode->right = new_node;
+        }
+    }else {
+        new_node->left = node->left;
+        node->left = new_node;
+    }
+
+    return 0;
+}
+
+TreeNode* BSTree::search(int d, TreeNode** parent) {
+    TreeNode *begin = root;
+    while((begin != nullptr) && (begin->val != d)) {
+        *parent = begin;
+        begin = (begin->val < d) ? begin->right : begin->left;
+    }   //Find the first one
+    return begin;
+}
+
+void BSTree::print() {
+    do_print(root);
+}
+
+void BSTree::do_print(TreeNode* begin) {
+    if(!begin) {
+        return;
+    }
+    cout<<"[";
+    do_print(begin->left);
+    cout<<" "<<begin->val<<" ";
+    do_print(begin->right);
+    cout<<"]";
+    return;
+}
+
+int main() {
+    BSTree tree;
+    vector<int> input = {6,3,1,0,4,8,9};
+    for(auto x : input) {
+        tree.insert(x);
+    }
+    tree.print();
+    cout<<endl;
+    Solution slt;
+    slt.flatten(tree.root);
+    tree.print();
+    return 0;
+}

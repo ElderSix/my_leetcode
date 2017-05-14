3b4099467ace5bd73aa22c33796c517146057db9
Techsailor He <techsailor@163.com>
2017-05-14 10:40:37 +0800

    Add new file

---
+++ tree/94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,125 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree [1,null,2,3],
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+*/
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+/**
+ * Definition for binary tree
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        travel(root, result);
+        return result;
+    }
+private:
+    void travel(TreeNode *root, vector<int> &result) {
+        if(!root) {
+            return;
+        }
+        travel(root->left, result);
+        result.push_back(root->val);
+        travel(root->right, result);
+    }
+};
+
+
+class BSTree {
+public:
+    BSTree():root(nullptr),size(0) {}
+    int insert(int d);
+    TreeNode* search(int d, TreeNode** parent);
+    int deleteNode(TreeNode* node);
+    void print();
+    TreeNode* root;
+private:
+    int size;
+    void do_print(TreeNode* begin);
+};
+
+int BSTree::insert(int d) {
+    TreeNode* pnode = nullptr;
+    TreeNode* new_node = new TreeNode(d);
+    TreeNode* node = search(d, &pnode);
+    if(!pnode) {
+        root = new_node;
+        return 0;
+    }
+    if(!node) {
+        if(d <= pnode->val) {
+            pnode->left = new_node;
+        }else {
+            pnode->right = new_node;
+        }
+    }else {
+        new_node->left = node->left;
+        node->left = new_node;
+    }
+
+    return 0;
+}
+
+TreeNode* BSTree::search(int d, TreeNode** parent) {
+    TreeNode *begin = root;
+    while((begin != nullptr) && (begin->val != d)) {
+        *parent = begin;
+        begin = (begin->val < d) ? begin->right : begin->left;
+    }   //Find the first one
+    return begin;
+}
+
+void BSTree::print() {
+    do_print(root);
+}
+
+void BSTree::do_print(TreeNode* begin) {
+    if(!begin) {
+        return;
+    }
+    cout<<"[";
+    do_print(begin->left);
+    cout<<" "<<begin->val<<" ";
+    do_print(begin->right);
+    cout<<"]";
+    return;
+}
+
+int main() {
+    BSTree tree;
+    vector<int> input = {6,3,1,0,4,8,7};
+    for(auto x : input) {
+        tree.insert(x);
+    }
+    tree.print();
+    cout<<endl;
+    Solution slt;
+    vector<int> ret = slt.inorderTraversal(tree.root);
+    for(auto x : ret) {
+        cout<<x<<",";
+    }
+    cout<<endl;
+    return 0;
+}

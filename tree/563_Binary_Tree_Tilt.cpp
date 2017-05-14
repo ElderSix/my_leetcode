f87bc0b9c3fad90b2aa87cca58307ff93fb8c3cc
Techsailor He <techsailor@163.com>
2017-05-14 20:55:43 +0800

    Add new file

---
+++ tree/563_Binary_Tree_Tilt.cpp
@@ -0,0 +1,134 @@
+/*
+Given a binary tree, return the tilt of the whole tree.
+
+The tilt of a tree node is defined as the absolute difference between the sum of all left
+subtree node values and the sum of all right subtree node values. Null node has tilt 0.
+
+The tilt of the whole tree is defined as the sum of all nodes' tilt.
+
+Example:
+Input:
+         1
+       /   \
+      2     3
+Output: 1
+Explanation:
+Tilt of node 2 : 0
+Tilt of node 3 : 0
+Tilt of node 1 : |2-3| = 1
+Tilt of binary tree : 0 + 0 + 1 = 1
+Note:
+
+The sum of node values in any subtree won't exceed the range of 32-bit integer.
+All the tilt values won't exceed the range of 32-bit integer.
+*/
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+/**
+ * Definition for binary tree
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        int tilt = 0;
+        travelForTilt(root, tilt);
+        return tilt;
+    }
+private:
+    int travelForTilt(TreeNode *root, int &tilt) {
+        if(!root) {
+            return 0;
+        }
+        int l_sum = travelForTilt(root->left, tilt);
+        int r_sum = travelForTilt(root->right, tilt);
+        tilt += ((l_sum - r_sum > 0) ? (l_sum - r_sum) : (r_sum - l_sum));
+        return root->val + l_sum + r_sum;
+    }
+};
+
+class BSTree {
+public:
+    BSTree():root(nullptr),size(0) {}
+    int insert(int d);
+    TreeNode* search(int d, TreeNode** parent);
+    int deleteNode(TreeNode* node);
+    void print();
+    TreeNode* root;
+private:
+    int size;
+    void do_print(TreeNode* begin);
+};
+
+int BSTree::insert(int d) {
+    TreeNode* pnode = nullptr;
+    TreeNode* new_node = new TreeNode(d);
+    TreeNode* node = search(d, &pnode);
+    if(!pnode) {
+        root = new_node;
+        return 0;
+    }
+    if(!node) {
+        if(d <= pnode->val) {
+            pnode->left = new_node;
+        }else {
+            pnode->right = new_node;
+        }
+    }else {
+        new_node->left = node->left;
+        node->left = new_node;
+    }
+
+    return 0;
+}
+
+TreeNode* BSTree::search(int d, TreeNode** parent) {
+    TreeNode *begin = root;
+    while((begin != nullptr) && (begin->val != d)) {
+        *parent = begin;
+        begin = (begin->val < d) ? begin->right : begin->left;
+    }   //Find the first one
+    return begin;
+}
+
+void BSTree::print() {
+    do_print(root);
+}
+
+void BSTree::do_print(TreeNode* begin) {
+    if(!begin) {
+        return;
+    }
+    cout<<"[";
+    do_print(begin->left);
+    cout<<" "<<begin->val<<" ";
+    do_print(begin->right);
+    cout<<"]";
+    return;
+}
+
+int main() {
+    BSTree tree, tree2;
+    vector<int> input = {3};
+    for(auto x : input) {
+        tree.insert(x);
+    }
+    tree.print();
+    cout<<endl;
+
+    Solution slt;
+    cout<<slt.findTilt(tree.root)<<endl;
+    return 0;
+}
